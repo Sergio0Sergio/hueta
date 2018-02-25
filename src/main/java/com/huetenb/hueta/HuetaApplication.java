20fff68cf41050a564759675a2e18f6757ef5198
Sergei Grimanov <tosergi@gmail.com>
2018-02-25 18:17:34 +0300

    1

---
+++ src/main/java/com/huetenb/hueta/HuetaApplication.java
@@ -0,0 +1,12 @@
+package com.huetenb.hueta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuetaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/huetenb/hueta/HuetaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huetenb.hueta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HuetaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
