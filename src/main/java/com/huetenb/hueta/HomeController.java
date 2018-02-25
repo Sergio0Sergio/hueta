c7bed2b5d30f3b42cd64b678f0190874901b399a
Sergei Grimanov <tosergi@gmail.com>
2018-02-25 19:08:21 +0300

    class HomeController was created

--- src/main/java/com/huetenb/hueta/HomeController.java
+++ src/main/java/com/huetenb/hueta/HomeController.java
@@ -1,11 +1,12 @@
 package com.huetenb.hueta;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	
+	@GetMapping
 	public String greetings (@RequestParam (required = false, defaultValue = "") String name) {
 		
 		return name.equals("") ? "Hey!" : "Hey, " + name + "!";
